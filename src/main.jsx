2a336b44df559736d9fa4d931abe20371e81ebe4
Your Name <zionubesie@gmail.com>
2025-01-17 17:10:40 +0100

    adjustment

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AssemblyApp from './AssemblyApp'
 // import App from './functionalProgramming/FetchApi.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AssemblyApp />
   </StrictMode>,
 )
