d10b9dc20fd3098974fa0796eed931f40b0ad36d
zion ubesie <zionubesie@gmail.com>
2025-01-18 14:21:25 +0100

    new change

--- src/AssemblyApp/Head.jsx
+++ src/AssemblyApp/Head.jsx
@@ -3,7 +3,7 @@ export default function Heading({ isGameLost, isGameWon }) {
     <main>
       <header>
         <h1> Guess The Word </h1>
-        <p>Guess the word within 8 attempts. You lose a life for each letter guess wrong</p>
+        <p>Guess the word within 8 attempts. You lose a life for each letter you guess wrong.</p>
       </header>
     </main>
 
